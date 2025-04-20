b46aa3386a5a63f99cf51fedc17cbb0209bd0cc1
Cees-Jan Kiewiet <github@ceesjankiewiet.nl>
2025-04-20 13:40:01 +0200

    Sort FUNCTION_LIST
    This makes the whole thing easier to scan

--- utils/update.php
+++ utils/update.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use WyriHaximus\React\PHPStan\Utils\Func;
 use WyriHaximus\React\PHPStan\Utils\ListFunctions;
 use WyriHaximus\React\PHPStan\Utils\Readme;
 use WyriHaximus\React\PHPStan\Utils\Rules\UseNonBlockingImplementationsRulePopulator;
@@ -9,5 +10,6 @@ use WyriHaximus\React\PHPStan\Utils\Rules\UseNonBlockingImplementationsRulePopul
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 $funcs = [...ListFunctions::listAllBlockingFunctions()];
+uasort($funcs, static fn (Func $left, Func $right): int => $left->name <=> $right->name);
 Readme::update(...$funcs);
 UseNonBlockingImplementationsRulePopulator::populate(...$funcs);
